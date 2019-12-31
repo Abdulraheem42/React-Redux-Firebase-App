a771d07cc45f1c398e6fa31b36ea7f4bbd7f1426
Abdulraheem42 <abdulraheem4255395@gmail.com>
2019-12-31 15:09:56 +0500

    create action and dispatch it..

--- some-articles/src/components/projects/CreateProject.js
+++ some-articles/src/components/projects/CreateProject.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
+import {connect} from "react-redux";
+import { createProject } from '../../store/actions/projectActions'
 
 class CreateProject extends Component {
     constructor(props){
@@ -19,11 +21,12 @@ class CreateProject extends Component {
 
     handleSubmit = (e)=>{
         e.preventDefault();
+        this.props.createProject(this.state);
+
         this.setState({
             title: '',
             content: ''
         });
-        console.log(this.state);
     };
 
     render() {
@@ -32,15 +35,15 @@ class CreateProject extends Component {
                 <form className='white' action="" onSubmit={this.handleSubmit.bind(this)}>
                 <h2 className="text-grey center">Create Project</h2>
                 <div className='row'>
-                    <div className='col l12 input-field'>
+                    <div className='col l12 s12 input-field'>
                         <label htmlFor="title">Title</label>
                         <input type="text" value={this.state.title} id='title' name='title' onChange={this.handleChange.bind(this)} />
                     </div>
-                    <div className='col l12 input-field'>
+                    <div className='col l12 s12 input-field'>
                         <label htmlFor="content">Content</label>
                         <input type="text" value={this.state.content} id='content' name='content' onChange={this.handleChange.bind(this)} />
                     </div>
-                    <div className='col l12 input-field center'>
+                    <div className='col l12 s12 input-field center'>
                         <button type='submit' className='btn btn-large blue z-depth-2'>Submit</button>
                     </div>
                 </div>
@@ -49,5 +52,10 @@ class CreateProject extends Component {
         );
     }
 }
+const mapDispatchToProps =(dispatch)=> {
+    return{
+        createProject: (project) => dispatch(createProject(project))
+    }
+};
 
-export default CreateProject;
+export default connect(null, mapDispatchToProps)(CreateProject);
--- some-articles/src/store/reducers/projectReducers.js
+++ some-articles/src/store/reducers/projectReducers.js
@@ -9,6 +9,10 @@ const initState = {
 };
 
 const projectReducers = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log("created Project", action.project)
+    }
     return state
 };
 
